327552edcafdb1799c62e1f9ec8cf99a02f7523b
Mohammed Mahmoud <mo7amedmb777@gmail.com>
2023-02-23 19:54:27 +0200

    add Cubit

--- lib/Features/Home/data/repos/home_repo_impl.dart
+++ lib/Features/Home/data/repos/home_repo_impl.dart
@@ -1,7 +1,7 @@
 import 'package:bookly_app/Features/Home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
-
+import '../../../../constants.dart';
 import '../../../../core/errors/failures.dart';
 import '../../../../core/utils/api_service.dart';
 import '../models/book_model/book_model.dart';
@@ -13,9 +13,7 @@ class HomeRepoImpl implements HomeRepo {
   @override
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
     try {
-      var data = await apiService.get(
-          endPoint:
-              'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming');
+      var data = await apiService.get(endPoint: newsetBooks);
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
@@ -39,8 +37,34 @@ class HomeRepoImpl implements HomeRepo {
   @override
   Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
     try {
+      var data = await apiService.get(endPoint: featuredBooks);
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      if (e is DioError) {
+        return left(
+          ServerFailure.fromDioError(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
+      {required String category}) async {
+    try {
       var data = await apiService.get(
-          endPoint: 'volumes?Filtering=free-ebooks&q=subject:Programming');
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=relevance &q=subject:Programming');
       List<BookModel> books = [];
       for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,8 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
+
+//EndPoints
+const newsetBooks =
+    'volumes?Filtering=free-ebooks&Sorting=newest &q=subject:Programming';
+const featuredBooks = 'volumes?Filtering=free-ebooks&q=subject:Programming';
